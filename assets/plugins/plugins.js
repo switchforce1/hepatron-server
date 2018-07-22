caa0bc9d9ce528e7a6941ef56414d501f4b19fbd
dadja <gnanzimdadja@yahoo.fr>
2018-07-22 22:31:57 +0200

    Webpack encore

--- assets/plugins/plugins.js
+++ assets/plugins/plugins.js
@@ -1,12 +1,10 @@
 const $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
-require('bootstrap');
-require('fastclick');
+
+//require.context('./extra_layout', false, /\.png$|.ico$/);
+require.context('./extra_layout/js');
+//require.context('./Ionicons/');
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
